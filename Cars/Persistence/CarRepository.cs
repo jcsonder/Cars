7970e8f62cb7dfa67ddb7fb821f6060b5784f7d1
jcsonder <jcsonder@gmail.com>
2016-03-25 16:21:14 +0100

    CarRepo

--- Cars/Persistence/CarRepository.cs
+++ Cars/Persistence/CarRepository.cs
@@ -20,17 +20,20 @@ namespace Cars.Persistence
             tasks.Add(Task.Factory.StartNew(() =>
             {
                 Car car = new SportCar("Porsche", "356", "silver");
+                base.CreateAsync(car);
                 ////Task.Delay(2000).Wait();
             }));
 
             tasks.Add(Task.Factory.StartNew(() =>
             {
                 Car car = new SportCar("VW", "Beetle", "purple");
+                CreateAsync(car);
             }));
 
             tasks.Add(Task.Factory.StartNew(() =>
             {
                 Car truck = new Truck("Ford", "F-350", "blue");
+                CreateAsync(truck);
             }));
 
             return Task.WhenAll(tasks);
