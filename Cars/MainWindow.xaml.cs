a02d013d05fcd7aed3a605acb61a5c10bd9f42b7
jcsonder <jcsonder@gmail.com>
2017-01-07 12:16:26 +0100

    Revert "Revert "Dispose + Sleep""
    This reverts commit 1608d54cdbeae198b57c23ea864d7b9f9b1f6220.

--- Cars/MainWindow.xaml.cs
+++ Cars/MainWindow.xaml.cs
@@ -2,8 +2,10 @@
 using Cars.Persistence;
 using Cars.Persistence.Helper;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Threading;
 using System.Windows;
 
 namespace Cars
@@ -14,15 +16,17 @@ namespace Cars
         private readonly IUnitOfWork _unitOfWork;
         private readonly IAsyncRepository<ICar> _carRepository;
         private readonly ICarService _carService;
-        private readonly List<IDisposable> _disposables;
+        private readonly SerialDisposable _disposable;
 
         public MainWindow()
         {
             _unitOfWork = new UnitOfWork();
             _carRepository = new CarRepository(_unitOfWork);
             _carService = new CarService(_carRepository);
-            _disposables = new List<IDisposable>();
+            _disposable = new SerialDisposable();
             InitializeComponent();
+
+            Debug.WriteLine("MainWindow initialized on threadId={0}", Thread.CurrentThread.ManagedThreadId);
         }
 
         private async void buttonCreateCars_Click(object sender, RoutedEventArgs e)
@@ -34,23 +38,29 @@ namespace Cars
 
         private void buttonGetCars_Click(object sender, RoutedEventArgs e)
         {
+            Debug.WriteLine("buttonGetCars_Click on threadId={0}", Thread.CurrentThread.ManagedThreadId);
+
             label.Content = "buttonGetCars clicked";
             listboxCars.Items.Clear();
 
             IObservable<ICar> dataObservable = _carService.GetAll();
-            // todo: dispose the disposable
 
-            _disposables.Add(dataObservable
-                .ObserveOnDispatcher()
+            _disposable.Disposable = dataObservable
+                .ObserveOnDispatcher()  // use ISchedulerProvider
                 .Subscribe(
-                    (d) => listboxCars.Items.Add(d),
-                    (ex) => label.Content = string.Format("OnException: {0}", ex.Message),
-                    () => label.Content = "OnCompleted"));
+                    d => 
+                        {
+                            Debug.WriteLine("OnNext on threadId={0}", Thread.CurrentThread.ManagedThreadId);
+                            Thread.Sleep(1000);
+                            listboxCars.Items.Add(d);
+                        },
+                    ex => label.Content = string.Format("OnException: {0}", ex.Message),
+                    () => label.Content = "OnCompleted");
         }
 
         protected override void OnClosed(EventArgs e)
         {
-            _disposables.ForEach(d => d.Dispose());
+            _disposable.Dispose();
         }
     }
 }
